83d4b6b75b9b8906bc64433a18155fe5877578c7
falovic <fatihsahin.dev@gmail.com>
2022-11-25 00:03:13 +0300

    components created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BtnWhiteComponent } from './shared/btn-white/btn-white.component';
+import { BtnColorComponent } from './shared/btn-color/btn-color.component';
+import { CasinoComponent } from './shared/casino/casino.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BtnWhiteComponent,
+    BtnColorComponent,
+    CasinoComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/btn-color/btn-color.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-btn-color',
+  templateUrl: './btn-color.component.html',
+  styleUrls: ['./btn-color.component.scss']
+})
+export class BtnColorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
